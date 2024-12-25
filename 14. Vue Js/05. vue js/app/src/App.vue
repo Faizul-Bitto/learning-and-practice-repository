dd18352954f21bd19406d19d8a05c8f07c72a480
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2024-12-25 23:21:21 +0600

    Practice files and codes

---
+++ 14. Vue Js/05. vue js/app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// importing the variables of 'data.js' file
+import { msg, year, person, getDate } from './data/data.js';
+</script>
+
+<template>
+  <!-- {{ }} -> 'Mustache Syntax' -->
+  <h1>{{ msg }}</h1>
+  <h2>Year is : {{ year }}</h2>
+  <!-- even we can pass any function in this 'Mustache Syntax' and can show output -->
+  <h2>Today is : {{ getDate() }}</h2>
+
+  <!-- as like object value catching system -> objectVariable.value, just like this write in the 'Mustache Syntax' -->
+  <h2>Name : {{ person.name }} Age : {{ person.age }}</h2>
+
+  <h2>You have total {{ person.tasks.length }} tasks today</h2>
+
+  <h2>Most important task is {{ person.tasks[0] }}</h2>
+</template>
+
+<style scoped></style>
---
+++ 14. Vue Js/05. vue js/app/src/data/data-2.js
@@ -0,0 +1,4 @@
+const msg = "Hello";
+
+// exporting the variable defined in this file with 'default' keyword
+export default msg;
