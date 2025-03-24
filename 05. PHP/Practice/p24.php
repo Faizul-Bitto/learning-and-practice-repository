9f5f6f4a7832123fb8934f2168da5f17666fda61
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-03-24 22:47:39 +0600

    scss practice codes and files

--- 05. PHP/Practice/p24.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// to ensure that an argument is passed as NULL to the parameter and no error occur, we use "?" before the type
-function worldCountryList( ...$country ) {
-
-    print_r( $country );
-}
-
-worldCountryList( "Bangladesh", "England", "USA", "Canada", "Iceland", "India", "Nepal", "Sri Lanka" );
--- 05. PHP/Practice/p26.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// to make sure to return a specific type, we use ':type'
-function addTwoNumber( $parameter ): int | float | String | bool | null {
-
-    return $parameter;
-}
-
-echo addTwoNumber( "A" );
