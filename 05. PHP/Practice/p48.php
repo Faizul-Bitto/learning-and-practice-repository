42aacdd450e3e0ce36dea52f323f667fe97a8296
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-03-24 22:44:11 +0600

    others practice codes and files

--- 05. PHP/Practice/p48.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$numbers = [21, 12, 23, 4, 55];
-
-$sorted = sort( $numbers );
-print_r( $numbers );
--- 05. PHP/Practice/p67.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$string = "     Hello World//%^&@#@@!#!@# ??    ";
-
-// we can remove any unnecessary elements from the string also
-echo trim( $string, " /%^&@#!?" ); // space, /, %, &, @, #, !, #, ? will be removed
