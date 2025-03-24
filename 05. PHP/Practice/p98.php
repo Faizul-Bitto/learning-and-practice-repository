1ad5cc2c3581962b64e5d4cdb29c5fcf51acb44d
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-03-24 22:46:57 +0600

    react.js practice codes and files

--- 05. PHP/Practice/p98.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// read the file
-$file = fopen( "books.txt", "r" );
-
-while ( !feof( $file ) ) {
-    echo fgets( $file );
-}
--- 13. Vue.js/vue js 1/02. my-app-practice-2/src/components/home/Testimonial.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-    <h3>child 5</h3>
-    <button>Testimonial</button>
-</template>
-
-<style scoped></style>
