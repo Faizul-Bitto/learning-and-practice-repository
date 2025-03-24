7f55abcba06c28e40dc35ba89f5b9aeec2566f59
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-03-24 22:45:42 +0600

    python practice codes and files

--- 05. PHP/Practice/p62.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$string = "Hello World";
-
-// to find positioning
-// method: substr( stringName, starting index, length or how many characters we want from the starting index )
-$result = substr( $string, 5, 5 );
-
-echo $result;
--- 05. PHP/Practice/p88.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// to start session:
-session_start();
-
-// $_SESSION["variable name"] = "value";
-$_SESSION["name"] = "John Doe";
-// SESSION is a super global variable, that means, from any other file we can access it.
