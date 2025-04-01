d9822c49f0dc894e8d45ccaad6959544e77d459c
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-04-01 08:57:00 +0600

    vue.js practice codes and files

--- Vue.js/vue.js part 4 - CSS class binding/css_style_binding/src/App.vue
+++ Vue.js/vue.js part 4 - CSS class binding/css_style_binding/src/App.vue
@@ -22,18 +22,26 @@ if ( 'error' == messageType ) {
 
 <template>
   <section class="container mx-auto">
-    <h1 class="text-center text-3xl py-12">CSS Style Binding</h1>
+    <h1 class="text-center text-3xl py-12">CSS Class Binding</h1>
 
     <section class="flex space-x-10">
-      <div class="w-1/3 h-16" :style="bgOrange"></div>
-      <div class="w-1/3 h-16" :style="bgIndigo"></div>
-      <div class="w-1/3 h-16" :style="bgGreen"></div>
+      <div class="w-1/3 h-16" :class="bgOrange"></div>
+      <div class="w-1/3 h-16" :class="bgIndigo"></div>
+      <div class="w-1/3 h-16" :class="bgGreen"></div>
     </section>
 
     <section class="mt-10">
       <div class="bg-red-600 h-16 flex items-center justify-center text-white"
         :class="[weekDay ? 'bg-green-600' : 'bg-red-600', weekDay ? 'text-4xl' : 'text-6xl']">
         It's a weekday
+      </div>
+    </section>
+
+    <section class="mt-10">
+      <div class=" border-t-4 rounded-b px-4 py-3 shadow-md"
+        :class="[colors1.bgColor, colors1.borderColor, colors1.textColor]">
+        <p class="font-bold">Our privacy policy has changed</p>
+        <p class="text-sm">Make sure you know how these changes affect you.</p>
       </div>
     </section>
   </section>
